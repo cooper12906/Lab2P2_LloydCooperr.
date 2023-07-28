b21f018eff6481dda88558897f8c627216e31247
Lloyd Cooper <cooperc913@gmail.com>
2023-07-28 13:21:28 -0600

    clases

--- src/lab2p2_lloydcooperr/Chefs.java
+++ src/lab2p2_lloydcooperr/Chefs.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab2p2_lloydcooperr;
 
-/**
- *
- * @author CUSTOMER PC
- */
 public class Chefs {
+    private String nombre;
+    private int edad;
+    private String turno;
+    private int numeroEstrellasMichelin;
+    private String sueldo;
+
+    public Chefs() {
+    }
+
+    public Chefs(String nombre, int edad, String turno, int numeroEstrellasMichelin, String sueldo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.turno = turno;
+        this.numeroEstrellasMichelin = numeroEstrellasMichelin;
+        this.sueldo = sueldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public int getNumeroEstrellasMichelin() {
+        return numeroEstrellasMichelin;
+    }
+
+    public void setNumeroEstrellasMichelin(int numeroEstrellasMichelin) {
+        this.numeroEstrellasMichelin = numeroEstrellasMichelin;
+    }
+
+    public String getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(String sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Chefs{" + "nombre=" + nombre + ", edad=" + edad + ", turno=" + turno + ", numeroEstrellasMichelin=" + numeroEstrellasMichelin + ", sueldo=" + sueldo + '}';
+    }
+    
     
 }
